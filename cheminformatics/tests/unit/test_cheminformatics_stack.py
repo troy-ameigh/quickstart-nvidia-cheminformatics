249f2004f9df575de33c5876c5b9bff22bd74690
Doruk Ozturk <dozturk89@gmail.com>
2022-01-19 00:25:32 -0500

    Fix unit tests

--- cheminformatics/tests/unit/test_cheminformatics_stack.py
+++ cheminformatics/tests/unit/test_cheminformatics_stack.py
@@ -1,3 +1,5 @@
+import os
+
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
@@ -5,11 +7,34 @@ from cheminformatics.cheminformatics_stack import CheminformaticsStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in cheminformatics/cheminformatics_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = CheminformaticsStack(app, "cheminformatics")
+def test_cheminformatics_stack_cluster_created():
+    app = core.App(
+        context={
+            "create_new_vpc": "True",
+            "existing_vpc_name": "SomeVpcName",
+            "cidr_block": "10.0.0.0/24",
+            "number_of_azs": 2,
+            "ec2_volume_size": 100,
+            "instance_type": "p3.2xlarge",
+            "cheminformatics_container": "public.ecr.aws/b9g4r0v3/cheminformatics_demo:0.1.2",
+            "megamolbart_container": "nvcr.io/nvidia/clara/megamolbart:0.1.2",
+            "megamolbart_model_url": "https://api.ngc.nvidia.com/v2/models/nvidia/clara/megamolbart/versions/0.1/zip",
+        }
+    )
+    stack = CheminformaticsStack(
+        app,
+        "cheminformatics",
+        env={
+            "region": os.getenv("CDK_DEFAULT_REGION"),
+            "account": os.getenv("CDK_DEFAULT_ACCOUNT"),
+        },
+    )
     template = assertions.Template.from_stack(stack)
-
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    template.has_resource_properties(
+        "AWS::ECS::Cluster",
+        {
+            "ClusterSettings": [
+                {"Name": "containerInsights", "Value": "enabled"}
+            ]
+        },
+    )
